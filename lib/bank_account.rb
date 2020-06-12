6034b0156c067808c2f65e88041d21bf7391650f
Aaron Feingold <ajfeingold88@gmail.com>
2020-06-12 21:41:23 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,9 +1,10 @@
 class BankAccount
-  attr_accessor :name  
+  attr_accessor :name
+  attr_reader :balance
   
   @@all = [] 
   
-  def initialize(name)
+  def initialize(name, balance)
     @name = name
     @balance = 1000
     @@all << self 
